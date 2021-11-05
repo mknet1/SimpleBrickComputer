026ebae724ec12001cecfe8923e2d6433e74a6ea
mknet1 <mknet@o2.pl>
2021-11-05 15:27:27 +0100

    Add files via upload

---
+++ SBC2/beeper.ino
@@ -0,0 +1,14 @@
+
+void beeper()
+{
+  if (up == LOW) {
+     digitalWrite(buzz, HIGH);
+    delay(10);
+     digitalWrite(buzz, LOW);
+    
+  }
+  if (down == LOW) {
+         digitalWrite(buzz, HIGH);
+     delay(10);
+     digitalWrite(buzz, LOW);}
+  }
